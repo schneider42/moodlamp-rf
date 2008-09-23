1557f5004cbd69bf0a63901a45da0301bf46e8d0
schneider <schneider@b5121046-f086-4304-a615-f695b2b065ff>
2008-09-23 01:04:20 +0000

    Changed standard baud rate

--- mld/src/mld.py
+++ mld/src/mld.py
@@ -436,7 +436,7 @@ class MLD:
         self.ml = MoodlampList()
         self.interfaces = []
         try:
-            self.interfaces.append(rf12interface.RF12Interface("/dev/ttyUSB0",115200,2,self))
+            self.interfaces.append(rf12interface.RF12Interface("/dev/ttyUSB0",230400,2,self))
         except:
             self.interfaces.append(rf12interface.RF12Interface("/dev/ttyS0",115200,2,self))
         #self.ml.append(Moodlamp(3,self.interfaces[0],self))
