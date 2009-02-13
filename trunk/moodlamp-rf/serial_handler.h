27a220c2d0b34fb7345a2f02e7e24ba63e552d51
schneider <schneider@b5121046-f086-4304-a615-f695b2b065ff>
2009-02-13 02:35:04 +0000

    Cleanup of serial handler

--- trunk/moodlamp-rf/serial_handler.h
+++ trunk/moodlamp-rf/serial_handler.h
@@ -3,6 +3,21 @@
 
 #include <stdint.h>
 #include <packet.h>
+#include "config.h"
+
+#define SERIAL_BUFFERLEN    270
+#define SERIAL_ESCAPE       'a'
+#define SERIAL_START        'c'
+#define SERIAL_END          'b'
+
+#define SERIAL_INIT         'I'
+#define SERIAL_RAWDATA      'r'
+
+enum SerialStates {
+    SERIAL_STATE_INIT,
+    SERIAL_STATE_NORMAL
+};
+
 void serial_putenc(uint8_t * d, uint16_t n);
 uint8_t serial_packetOut(struct packet_t * p);
 uint8_t serial_packetIn(struct packet_t * p);
