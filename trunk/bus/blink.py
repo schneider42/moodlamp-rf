94f155c4bb5c6d3b88b93d56bb454e6cff0b50b7
schneider <schneider@b5121046-f086-4304-a615-f695b2b065ff>
2009-11-12 22:26:15 +0000

    code clearup

--- trunk/bus/blink.py
+++ trunk/bus/blink.py
@@ -1,16 +1,8 @@
-import cherrypy
 import lamplib
 import time
-import threading
-import thread
-
 while 1:
     lamplib.setcolor("00",0,0,255)
-    time.sleep(0.1)
-    lamplib.setcolor("00",0,0,0)
-    time.sleep(0.1)
-    lamplib.setcolor("00",0,0,255)
-    time.sleep(0.1)
+    time.sleep(0.5)
     lamplib.setcolor("00",0,0,0)
     time.sleep(0.5)
 
