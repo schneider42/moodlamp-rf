30d4fc54eb781d2c2895b93d53f2efe6d89b3394
schneider <schneider@b5121046-f086-4304-a615-f695b2b065ff>
2009-11-28 15:34:45 +0000

    police wave

--- trunk/bus/wave2.py
+++ trunk/bus/wave2.py
@@ -1,13 +1,16 @@
 import lamplib
 import time
 
+speed = 50.
+
 def fadeOn(lamp):
-    lamplib.fade(lamp,255,255,0,200)
+    lamplib.fade(lamp,255,255,0,200*speed)
 
 def fadeOff(lamp):
-    lamplib.fade(lamp,0,0,255,200)
+    lamplib.fade(lamp,0,0,255,200*speed)
 
-lamps = ('33','4F','32','31','30','51')
+lamps = ('31','30','51','33','4F','32')
+#,'12','21','1B','2A','14','1A')
 lamp = 0
 lamplib.setcolor('FE',0,0,255);
 while 1:
@@ -15,13 +18,13 @@ while 1:
     lamp+=1
     if lamp == len(lamps):
         lamp = 0
-    lamplib.fade(lamps[lamp],0,0,127,400)
-    time.sleep(0.25)
-    lamplib.fade(lamps[lamp],255,0,0,400)
-    time.sleep(0.25)
+    lamplib.fade(lamps[lamp],0,0,127,400*speed)
+    time.sleep(0.25/speed)
+    lamplib.fade(lamps[lamp],255,0,0,400*speed)
+    time.sleep(0.25/speed)
 
-    fadeOn(lamps[lamp])
-    time.sleep(5)
+#    fadeOn(lamps[lamp])
+    time.sleep(2/speed)
 
 
 
