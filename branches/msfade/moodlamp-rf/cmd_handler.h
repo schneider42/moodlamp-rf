0e91310ec1ae2c1b60f232cee9d6f9bf4bb83633
schneider <schneider@b5121046-f086-4304-a615-f695b2b065ff>
2010-01-09 19:53:51 +0000

    added msfade branch

---
+++ branches/msfade/moodlamp-rf/cmd_handler.h
@@ -0,0 +1,75 @@
+/* vim:fdm=marker ts=4 et ai
+ * {{{
+ *         moodlamp-ng - fnordlicht firmware next generation
+ *
+ *    for additional information please
+ *    see http://blinkenlichts.net/
+ *    and http://koeln.ccc.de/prozesse/running/fnordlicht
+ *
+ * This is a modified version of the fnordlicht
+ * (c) by Alexander Neumann <alexander@bumpern.de>
+ *     Lars Noschinski <lars@public.noschinski.de>
+ *
+ * Modifications done by Tobias Schneider(schneider@blinkenlichts.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ }}} */
+#ifndef __CMD_HANDLER_H_
+#define __CMD_HANDLER_H_
+
+#include "static_scripts.h"
+#define CMD_POWER               0x10
+#define CMD_BRIGHTNESS_UP       0x11
+#define CMD_BRIGHTNESS_DOWN     0x12
+#define CMD_FASTER              0x13
+#define CMD_SLOWER              0x14
+#define CMD_FULL_BRIGHTNESS     0x15
+#define CMD_ZERO_BRIGHTNESS     0x16
+#define CMD_PAUSE               0x17
+#define CMD_SAVE                0x18
+#define CMD_SLEEP               0x19
+#define CMD_COLOR_UP            0x1A
+#define CMD_COLOR_DOWN          0x1B
+#define CMD_RED                 0x1C
+#define CMD_GREEN               0x1D
+#define CMD_BLUE                0x1E
+#define CMD_COLOR_FULL          0x1F
+#define CMD_COLOR_ZERO          0x20
+#define CMD_SET_SCRIPT          0x21
+#define CMD_GET_VERSION         'V'
+#define CMD_GET_STATE           'G'
+#define CMD_SET_SPEED           's'
+#define CMD_SET_BRIGHTNESS      'D'
+#define CMD_SET_STATE           'S'
+#define CMD_SET_COLOR           'C'
+#define CMD_RESET               'r'
+#define CMD_SERVER_SET          'x'
+#define CMD_SETUP_OK            'O'
+#define CMD_SET_NAME            'N'
+#define CMD_RAW                 'W'
+#define CMD_DISABLE_BEACON      'Q'
+#define CMD_SELF_ASSIGN         'A'
+#define CMD_STANDALONE          'X'
+#define CMD_FADE                'F'
+#define CMD_FADEMS              'M'
+#define CMD_GET_VOLTAGE         'K'
+
+
+uint8_t cmd_interpret(uint8_t * cmd, uint8_t * result);
+uint8_t cmd_handler(uint8_t cmd, uint8_t * param, uint8_t * result);
+void cmd_setscript(void (*execute)(struct thread_t *current_thread), uint16_t position);
+#endif
---
+++ branches/msfade/moodlamp-rf/leds.c
@@ -0,0 +1,66 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "leds.h"
+#include "config.h"
+#include "pinutils.h"
+
+void leds_init(void)
+{
+    DDR_CONFIG_OUT(LED1); 
+    DDR_CONFIG_OUT(LED2); 
+    DDR_CONFIG_OUT(LED3); 
+    PIN_SET(LED2);
+}
+
+inline void leds_rx(void)
+{
+    uint8_t sreg = SREG; cli();
+    PIN_SET(LED2);
+    SREG = sreg;
+}
+
+
+inline void leds_rxend(void)
+{
+    uint8_t sreg = SREG; cli();
+    PIN_CLEAR(LED2);
+    SREG = sreg;
+}
+
+inline void leds_adc(void)
+{
+//    uint8_t sreg = SREG; cli();
+//    PIN_SET(LED1);
+//    SREG = sreg;
+}
+
+
+inline void leds_adcend(void)
+{
+//    uint8_t sreg = SREG; cli();
+//    PIN_CLEAR(LED1);
+//    SREG = sreg;
+}
+
+inline void leds_tx(void)
+{
+    uint8_t sreg = SREG; cli();
+    PIN_SET(LED3);
+    SREG = sreg;
+}
+
+
+inline void leds_txend(void)
+{
+    uint8_t sreg = SREG; cli();
+    PIN_CLEAR(LED3);
+    SREG = sreg;
+}
+
+inline void leds_main(void)
+{
+    uint8_t sreg = SREG; cli();
+    PIN_TOGGLE(LED1);
+    SREG = sreg;
+}
