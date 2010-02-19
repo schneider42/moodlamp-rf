07f62c41ed63aeee54a625278c7650af8a738186
joe <joe@b5121046-f086-4304-a615-f695b2b065ff>
2010-02-19 19:27:10 +0000

    mld:     added gpl licence headers and file

--- mld/src/moodlamp.py
+++ mld/src/moodlamp.py
@@ -1,3 +1,20 @@
+ # Copyright (c) 2008 by Tobias Schneider <schneider@blinkenlichts.net>
+ # Copyright (c) <year> by your name <your@email>
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import threading        #for RLock 
 
 class MoodlampList(list):
