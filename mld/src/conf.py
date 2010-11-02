55705f552d550878c98329a9b8501c0862912401
9R <9R@b5121046-f086-4304-a615-f695b2b065ff>
2010-11-02 21:00:26 +0000

    moved settings from mld.py to conf.py

---
+++ mld/src/conf.py
@@ -0,0 +1,11 @@
+#class CONF:
+daemonize = 'false' #true/false
+serial = "/dev/ttyUSB0"
+force = "true"
+pidfile = "/tmp/mld.pid"
+log = "false"
+port = int(2324)
+
+#   def data(self, data, broadcast):
+#      pass
+
